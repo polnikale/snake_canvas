e53d710b3b3f881adb17ac4a1bee01dcdb29cdb2
polnikale <nikpolale@gmail.com>
2018-03-10 00:21:05 +0200

    simple beginning. Hooray, our snake moves!!!

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 const canvas = document.getElementById('snake');
 let ctx = canvas.getContext('2d');
-ctx.moveTo(0,0);
-ctx.lineTo(500,500);
-ctx.stroke();
-ctx.moveTo(0,500);
-ctx.lineTo(500,0);
-ctx.stroke();
+
+let Snake = function(length, heightBlock, startPos) {
+  this.snakeLength = length;
+  this.heightBlock = heightBlock;
+  this.x = startPos.x;
+  this.y = startPos.y;
+};
+Snake.prototype.draw = function() {
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, 500, 500);
+  for (let counter = 0; counter < this.snakeLength; counter++) {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(counter*this.heightBlock+this.x,this.y,this.heightBlock,this.heightBlock);
+  }
+}
+Snake.prototype.init = function() {
+  this.draw();
+  setInterval(() => {
+    this.moveSnake(); //arrow function in order to protect scope
+  }, 100);
+};
+Snake.prototype.moveSnake = function() {
+  this.x += this.heightBlock;
+  this.draw();
+};
+
+let snake = new Snake(4,25, {x: 100, y: 100});
+snake.init();
